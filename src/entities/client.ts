25de506162ca50e0a0f04c5f485c1d4ad43a533a
Cedric Haase <cedric@haase-online.com>
2019-07-21 12:50:11 +0200

    Add address field to client

--- src/entities/client.ts
+++ src/entities/client.ts
@@ -1,8 +1,11 @@
 export class Client {
     cn: number;
     name: string;
+    address: string;
 
     constructor(
         public peer: any // ENetPeer
-    ) {}
+    ) {
+        this.address = peer.address().address;
+    }
 }
