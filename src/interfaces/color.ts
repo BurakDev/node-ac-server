f0c7a60bf4255913f3e2a0d84a64a3ce307d4901
Cedric Haase <cedric@haase-online.com>
2019-07-21 18:35:14 +0200

    Add Color enum

---
+++ src/interfaces/color.ts
@@ -0,0 +1,45 @@
+export enum Color {
+    Green = 0,
+    Blue,
+    Yellow,
+    Red,
+    Gray,
+    White,
+    DarkBrown,
+    DarkRed,
+    Magenta,
+    Orange,
+
+    Red0 = 'A',
+    Red1 = 'B',
+    Red2 = 'C',
+    Red3 = 'D',
+
+    Yellow0 = 'E',
+    Yellow1 = 'F',
+    Yellow2 = 'G',
+    Yellow3 = 'H',
+
+    Green0 = 'I',
+    Green1 = 'J',
+    Green2 = 'K',
+    Green3 = 'L',
+
+    Cyan0 = 'M',
+    Cyan1 = 'N',
+    Cyan2 = 'O',
+    Cyan3 = 'P',
+
+    Blue0 = 'Q',
+    Blue1 = 'R',
+    Blue2 = 'S',
+    Blue3 = 'T',
+
+    Magenta0 = 'U',
+    Magenta1 = 'V',
+    Magenta2 = 'W',
+    Magenta3 = 'X',
+
+    LightGray = 'Y',
+    DarkGray = 'Z'
+}
