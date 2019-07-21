296e6da189942b1606ec5f1da8b2679b0483c7a1
Cedric Haase <cedric@haase-online.com>
2019-07-21 20:34:21 +0200

    Add skin and team fields to Client

--- src/entities/client.ts
+++ src/entities/client.ts
@@ -1,7 +1,13 @@
+import {Team} from "../interfaces/team";
+
 export class Client {
     cn: number;
     name: string;
     address: string;
+
+    skinCLA = 3;
+    skinRVSF = 0;
+    team = Team.TEAM_SPECT;
 
     constructor(
         public peer: any // ENetPeer
