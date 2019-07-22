1e3c11ec76aaa0cccaaa44df6a2d7db504ecd5ca
Cedric Haase <cedric@haase-online.com>
2019-07-22 23:17:14 +0200

    Add Gun and SpawnPermission enums

---
+++ src/entities/client-game-state.ts
@@ -0,0 +1,12 @@
+import {ClientState} from "../interfaces/client-state";
+import {Gun} from "../interfaces/gun";
+
+export class ClientGameState {
+    state = ClientState.CS_SPECTATE;
+    selectedGun = Gun.GUN_KNIFE;
+
+    lifeSequence = 0;
+    health = 1; // OSOK 4 eva
+    armor = 0;
+    
+}
---
+++ src/interfaces/spawn-permission.ts
@@ -0,0 +1,8 @@
+export enum SpawnPermission {
+    SP_OK = 0,
+    SP_REFILLMATCH,
+    SP_OK_NUM,
+    SP_WRONGMAP,
+    SP_SPECT,
+    SP_NUM
+}
